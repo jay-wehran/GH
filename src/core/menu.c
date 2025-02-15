8f7d529838d4318d32828198af5c283c44b19afa
Jason Wehran <jtw424@lehigh.edu>
2025-02-14 20:45:05 -0500

    menu + makefile

---
+++ src/core/menu.c
@@ -0,0 +1,8 @@
+#include "stdio.h"
+
+#include "inc/menu.h"
+
+void menu_init() {
+    printf("Golf Handicap Index System\nBy Jay\n\n");
+    printf("1. Post Score\n2. View Recent Scores\n3. Golfer Lookup\n4. Handicap Calculator");
+}
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "inc/menu.h"
+
+
+int main(void) {
+
+    menu_init();
+    return 0;
+}
